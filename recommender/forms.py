74954e3d8da463c7fdf55a69a0f2aacac7d5b963
Rishabh Agarwal <45998880+RishabhAgarwal-2001@users.noreply.github.com>
2023-07-05 08:46:12 +0530

    Update forms.py

--- recommender/forms.py
+++ recommender/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
 class MovieSearchForm(forms.Form):
-    imdb_rating = forms.DecimalField(min_value=0.1, max_value=9.9, required=True, label='Minimum IMDB Rating')
+    imdb_rating = forms.DecimalField(min_value=0.0, max_value=9.9, label='Minimum IMDB Rating')
+    rotten_tomato_rating = forms.DecimalField(min_value=0.0, max_value=9.9, label='Minimum Rotten Tomato Rating')
+    include_empty_rotten_tomato_ratings = forms.BooleanField(required=False, initial=False)
+    include_empty_imdb_ratings = forms.BooleanField(required=False, initial=False)
     release_year = forms.IntegerField(required=True)
